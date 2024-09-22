60de626ac1b3f1d539275d0f82c080456041062c
ashay.wakode <ashay.wakode@tii.ae>
2024-09-22 15:29:02 +0400

    Update b star

--- algorithms/b_astar.py
+++ algorithms/b_astar.py
@@ -3,7 +3,9 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
-import igraph as ig
+
+from SKC.workspace.workspace import Workspace
+from SKC.workspace.obstacle import Obstacle
 
 # Current file's directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,115 +18,65 @@ ax.set_axis_off()
 
 
 class BAStarRoute:
-    def __init__(self, side_size, obstacle, plot_flag, animate_flag):
-        self.side_size = side_size
-        self.grid_size = self.side_size * self.side_size
-        self.obstacle_list = obstacle
+    def __init__(self, workspace, plot_flag, animate_flag):
+        self.workspace = workspace
         self.plot_flag = plot_flag
         self.animate_flag = animate_flag
-
-        # Generate Graph
-        self.g = ig.Graph(n=self.grid_size)
-        self.subgraph = ig.Graph(n=self.grid_size)
-        self.subgraph.vs["name"] = [str(i) for i in range(self.grid_size)]
-        self.subgraph.vs["label"] = self.subgraph.vs["name"]
 
         self.obstacle_detected = []
         self.all_blocked = False
 
-        # Area covered by the Hilbert's curve
-        self.xmin = 0
-        self.ymin = 0
-        # Grid size
-        self.grid = 1
-
-        # Points
-        self.points = {}
-
-        # Populate point object
-        self.get_coordinates()
-        self.coordinate = np.array(list(self.points.values()))
-
-        self.x_nom = self.coordinate[:, 0]
-        self.y_nom = self.coordinate[:, 1]
-
-        self.x_visited = np.array([self.x_nom[0]])
-        self.y_visited = np.array([self.y_nom[0]])
+        self.x_visited = [self.workspace.x_nom[0]]
+        self.y_visited = [self.workspace.y_nom[0]]
         self.points_visited = [0]
 
         self.overall_path = []
         self.bastar_complete = False
-        self.generate_graph()
         self.get_alternate_path()
 
         self.agent, = ax.plot([], [], 'o', color='green')
         self.path, = ax.plot([], [], 'g-', linewidth=2)
 
-
-    def get_coordinates(self):
-        self.points[0] = np.array([self.xmin, self.ymin])
-
-        for i in range(1, self.grid_size):
-            col_count = int(i / self.side_size)
-            row_count = i % self.side_size
-
-            if row_count == 0:
-                self.points[i] = np.array([self.points[i - 1][0] + self.grid, self.points[i - 1][1]])
-            else:
-                if col_count % 2 == 0:
-                    self.points[i] = np.array([self.points[i - 1][0], self.points[i - 1][1] + self.grid])
-                else:
-                    self.points[i] = np.array([self.points[i - 1][0], self.points[i - 1][1] - self.grid])
-
     def get_point_index(self, xl, yl):
         """Calculates the index of the point on the hilbert's curve"""
-        x_index = np.where(self.x_nom == xl)[0]
-        y_index = np.where(self.y_nom == yl)[0]
+        x_index = np.where(self.workspace.x_nom == xl)[0]
+        y_index = np.where(self.workspace.y_nom == yl)[0]
         return list(np.intersect1d(x_index, y_index))[0]
 
     def get_adjacent_nodes(self, i):
         """Outputs the adjacent nodes of a given node"""
-        x_i = self.x_nom[i]
-        y_i = self.y_nom[i]
+        x_i = self.workspace.x_nom[i]
+        y_i = self.workspace.y_nom[i]
 
         adjacent_points = {
-            "p1": [x_i + self.grid, y_i],
-            "p2": [x_i, y_i + self.grid],
-            "p3": [x_i - self.grid, y_i],
-            "p4": [x_i, y_i - self.grid],
+            "p1": [x_i + self.workspace.grid, y_i],
+            "p2": [x_i, y_i + self.workspace.grid],
+            "p3": [x_i - self.workspace.grid, y_i],
+            "p4": [x_i, y_i - self.workspace.grid],
         }
 
         adjacent_nodes = [
             self.get_point_index(adjacent_points[i][0], adjacent_points[i][1])
-            if adjacent_points[i][0] in self.x_nom and adjacent_points[i][1] in self.y_nom
+            if adjacent_points[i][0] in self.workspace.x_nom and adjacent_points[i][1] in self.workspace.y_nom
             else -1
             for i in adjacent_points
         ]
 
         return adjacent_nodes
 
-    def generate_graph(self):
-        # graph Generation
-        for i in range(self.grid_size):
-            neigh = self.get_adjacent_nodes(i)
-            # neigh = self.adj_dict[i]
-            for j in range(4):
-                if neigh[j] != -1 and self.g.are_connected(i, neigh[j]) == False:
-                    self.g.add_edge(i, neigh[j])
-
     def get_rel_direction(self, origin_point, adjacent_points):
         rel_directions = {}
 
         for point in adjacent_points:
             if point != -1 and point not in self.obstacle_detected:
-                if point not in self.obstacle_list:
-                    if self.y_nom[point] > self.y_nom[origin_point]:
+                if point not in self.workspace.obstacles:
+                    if self.workspace.y_nom[point] > self.workspace.y_nom[origin_point]:
                         rel_directions['north'] = point
-                    elif self.y_nom[point] < self.y_nom[origin_point]:
+                    elif self.workspace.y_nom[point] < self.workspace.y_nom[origin_point]:
                         rel_directions['south'] = point
-                    elif self.x_nom[point] < self.x_nom[origin_point]:
+                    elif self.workspace.x_nom[point] < self.workspace.x_nom[origin_point]:
                         rel_directions['west'] = point
-                    elif self.x_nom[point] > self.x_nom[origin_point]:
+                    elif self.workspace.x_nom[point] > self.workspace.x_nom[origin_point]:
                         rel_directions['east'] = point
                 else:
                     self.obstacle_detected.append(point)
@@ -149,23 +101,23 @@ class BAStarRoute:
         """Outputs all the adjacent nodes of a given node"""
         dir = ['east', 'north-east', 'north', 'north-west', 'west', 'south-west', 'south', 'south-east']
 
-        x_i = self.x_nom[point]
-        y_i = self.y_nom[point]
+        x_i = self.workspace.x_nom[point]
+        y_i = self.workspace.y_nom[point]
 
         adjacent_points = {
-            dir[0]: [x_i + self.grid, y_i],
-            dir[1]: [x_i + self.grid, y_i + self.grid],
-            dir[2]: [x_i, y_i + self.grid],
-            dir[3]: [x_i - self.grid, y_i + self.grid],
-            dir[4]: [x_i - self.grid, y_i],
-            dir[5]: [x_i - self.grid, y_i - self.grid],
-            dir[6]: [x_i, y_i - self.grid],
-            dir[7]: [x_i + self.grid, y_i - self.grid],
+            dir[0]: [x_i + self.workspace.grid, y_i],
+            dir[1]: [x_i + self.workspace.grid, y_i + self.workspace.grid],
+            dir[2]: [x_i, y_i + self.workspace.grid],
+            dir[3]: [x_i - self.workspace.grid, y_i + self.workspace.grid],
+            dir[4]: [x_i - self.workspace.grid, y_i],
+            dir[5]: [x_i - self.workspace.grid, y_i - self.workspace.grid],
+            dir[6]: [x_i, y_i - self.workspace.grid],
+            dir[7]: [x_i + self.workspace.grid, y_i - self.workspace.grid],
         }
 
         adjacent_nodes = {
             direction: self.get_point_index(coord[0], coord[1])
-            if coord[0] in self.x_nom and coord[1] in self.y_nom
+            if coord[0] in self.workspace.x_nom and coord[1] in self.workspace.y_nom
             else -1
             for direction, coord in adjacent_points.items()
         }
@@ -192,20 +144,19 @@ class BAStarRoute:
 
     def get_subgraph(self):
         for i in self.points_visited:
-            if i not in self.subgraph.vs["name"]:
+            if i not in self.workspace.subgraph.vs["name"]:
                 neigh = self.get_adjacent_nodes(i)
-                # neigh = self.adj_dict[i]
                 for j in range(4):
                     if (
                             neigh[j] != -1
-                            and self.subgraph.are_connected(i, neigh[j]) == False
+                            and self.workspace.subgraph.are_connected(i, neigh[j]) == False
                             and neigh[j] in self.points_visited
                     ):
-                        self.subgraph.add_edge(i, neigh[j])
+                        self.workspace.subgraph.add_edge(i, neigh[j])
 
     def get_shortest_dist(self, start, end):
         self.get_subgraph()
-        shortest_path = self.subgraph.get_shortest_path(start, to=end, output='vpath')
+        shortest_path = self.workspace.subgraph.get_shortest_path(start, to=end, output='vpath')
         return shortest_path, len(shortest_path) - 1
 
     def get_backtrack_path(self, last_bpath):
@@ -266,72 +217,62 @@ class BAStarRoute:
         self.overall_path.append(self.points_visited[prev_len:])
 
     def plot_workspace(self):
-        self.x_bound = [min(self.x_nom) - self.grid / 2, min(self.x_nom) - self.grid / 2, max(self.x_nom) +
-                        self.grid / 2, max(self.x_nom) + self.grid / 2, min(self.x_nom) - self.grid / 2]
-        self.y_bound = [min(self.y_nom) - self.grid / 2, max(self.y_nom) + self.grid / 2, max(self.y_nom) +
-                        self.grid / 2, min(self.y_nom) - self.grid / 2, min(self.y_nom) - self.grid / 2]
+        self.x_bound = [min(self.workspace.x_nom) - self.workspace.grid / 2,
+                        min(self.workspace.x_nom) - self.workspace.grid / 2, max(self.workspace.x_nom) +
+                        self.workspace.grid / 2, max(self.workspace.x_nom) + self.workspace.grid / 2,
+                        min(self.workspace.x_nom) - self.workspace.grid / 2]
+        self.y_bound = [min(self.workspace.y_nom) - self.workspace.grid / 2,
+                        max(self.workspace.y_nom) + self.workspace.grid / 2, max(self.workspace.y_nom) +
+                        self.workspace.grid / 2, min(self.workspace.y_nom) - self.workspace.grid / 2,
+                        min(self.workspace.y_nom) - self.workspace.grid / 2]
 
-        for i in range(self.grid_size):
-            if i in self.obstacle_detected:
-                rectangle = plt.Rectangle(
-                    (self.x_nom[i] - self.grid / 2, self.y_nom[i] - self.grid / 2),
-                    1,
-                    1,
-                    fc="red",
-                    alpha=0.2,
-                    ec="black",
-                )
-                plt.gca().add_patch(rectangle)
-            else:
-                rectangle = plt.Rectangle(
-                    (self.x_nom[i] - self.grid / 2, self.y_nom[i] - self.grid / 2),
-                    1,
-                    1,
-                    fc="grey",
-                    alpha=0.04,
-                    ec="black",
-                )
-                plt.gca().add_patch(rectangle)
+        rectangles = []
+        for i in range(self.workspace.grid_size):
+            color = "red" if i in self.obstacle_detected else "grey"
+            alpha = 0.2 if i in self.obstacle_detected else 0.04
+            rectangles.append(plt.Rectangle(
+                (self.workspace.x_nom[i] - self.workspace.grid / 2,
+                 self.workspace.y_nom[i] - self.workspace.grid / 2),
+                1, 1, fc=color, alpha=alpha, ec="black"
+            ))
+        for rect in rectangles:
+            plt.gca().add_patch(rect)
 
     def plot(self):
         self.plot_workspace()
 
-        self.node_coord = [[self.x_nom[i], self.y_nom[i]] for i in range(self.grid_size)]
-        self.visual_style = {}
-        self.g.vs["name"] = [str(i) for i in range(self.grid_size)]
+        for i in self.points_visited:
+            self.x_visited.append(self.workspace.x_nom[i])
+            self.y_visited.append(self.workspace.y_nom[i])
 
-        self.visual_style["edge_width"] = [0.3]
-        self.visual_style["vertex_size"] = 10
-        self.visual_style["edge_color"] = "orange"
-        self.layout_subgraph = ig.Layout(coords=self.node_coord)
+        plt.plot(self.x_visited[0], self.y_visited[0], marker="o", markersize=10, markeredgecolor="blue",
+                 markerfacecolor="blue")
+        plt.plot(self.x_visited[-1], self.y_visited[-1], marker="o", markersize=10, markeredgecolor="gold",
+                 markerfacecolor="gold")
 
-        ig.plot(self.g, target=ax, layout=self.layout_subgraph, **self.visual_style)
-
-        x = list([self.x_nom[i] for i in self.points_visited])
-        y = list([self.y_nom[i] for i in self.points_visited])
-        print(self.points_visited)
         for i in range(len(self.overall_path)):
             if i % 2 == 0:
-                ax.text(self.x_nom[self.overall_path[i][0]], self.y_nom[self.overall_path[i][0]], f'{int(i / 2) + 1},S',
+                plt.text(self.workspace.x_nom[self.overall_path[i][0]], self.workspace.y_nom[self.overall_path[i][0]],
+                        f'{int(i / 2) + 1},S',
                         fontsize=24, ha='left', va='top',
                         color='red')
 
-                ax.text(self.x_nom[self.overall_path[i][-1]], self.y_nom[self.overall_path[i][-1]],
+                plt.text(self.workspace.x_nom[self.overall_path[i][-1]], self.workspace.y_nom[self.overall_path[i][-1]],
                         f'{int(i / 2) + 1},E',
                         fontsize=24, ha='left', va='top',
                         color='blue')
 
-        for i, (x, y) in self.points.items():
-            ax.text(x, y, f'{i}', fontsize=24, ha='right', va='bottom', color='blue')
+        # for i, (x, y) in self.workspace.points.items():
+        #     plt.text(x, y, f'{i}', fontsize=24, ha='right', va='bottom', color='blue')
 
         if self.plot_flag:
-            plt.plot(x, y, linestyle="solid", color="green", linewidth=1.0)
+            plt.plot(self.x_visited, self.y_visited, linestyle="solid", color="green", linewidth=1.0)
             plt.show()
 
         if self.animate_flag:
             ani = animation.FuncAnimation(fig, self.motion_update, frames=len(self.points_visited),
                                           interval=100, blit=True)
-            ani.save(os.path.join(doc_anim_dir, 'animation.gif'), writer='ffmpeg', fps=5, dpi=300)
+            ani.save(os.path.join(doc_anim_dir, 'bstar_animation.gif'), writer='ffmpeg', fps=5, dpi=300)
 
     def init(self):
         self.agent.set_data([], [])
@@ -339,13 +280,16 @@ class BAStarRoute:
         return self.agent, self.path
 
     def motion_update(self, i):
-        self.agent.set_data(self.x_nom[self.points_visited[i]], self.y_nom[self.points_visited[i]])
-        self.path.set_data(self.x_nom[:self.points_visited[i] + 1], self.y_nom[:self.points_visited[i] + 1])
+        self.agent.set_data(self.workspace.x_nom[self.points_visited[i]], self.workspace.y_nom[self.points_visited[i]])
+        self.path.set_data(self.workspace.x_nom[:self.points_visited[i] + 1],
+                           self.workspace.y_nom[:self.points_visited[i] + 1])
         return self.agent, self.path
 
 
 if __name__ == "__main__":
-    obs = np.array([9, 22, 25, 34, 45, 50, 61, 46, 47, 33, 32, 26, 27, 28])
-    # obs = np.array([])
-    bastar = BAStarRoute(10, obs, True, False)
+    iteration = 3
+    side_size = 2 ** iteration
+    obstacle = Obstacle(side_size, 0.2, 0.5)
+    boustro_workspace = Workspace('boustro', iteration, obstacle)
+    bastar = BAStarRoute(boustro_workspace, True, False)
     bastar.plot()
