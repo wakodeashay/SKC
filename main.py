5e8c723658f471a776bf766ee08b1603e26863b6
ashay.wakode <ashay.wakode@tii.ae>
2024-09-22 15:30:38 +0400

    Update main

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import numpy as np
 from algorithms.hilbert_coverage import HilbertRoute
-# import time
+from algorithms.b_astar import BAStarRoute
+from workspace.workspace import Workspace
+from workspace.obstacle import Obstacle
 
 
-obs = np.array([3, 4, 9, 34])
-hilbert_route = HilbertRoute(2, obs, True, True)
+iteration = 3
+side_size = 2 ** iteration
+obstacle = Obstacle(side_size, 0.6,1.0)
+
+hilbert_workspace = Workspace('hilbert', iteration, obstacle)
+boustro_workspace = Workspace('boustro', iteration, obstacle)
+
+hilbert_route = HilbertRoute(hilbert_workspace, -1, True, False)
 hilbert_route.plot()
+
+bastar = BAStarRoute(boustro_workspace, True, False)
+bastar.plot()
+
+print(hilbert_route.points_visited)
+print(bastar.points_visited)
+print(len(hilbert_route.points_visited))
+print(len(bastar.points_visited))
